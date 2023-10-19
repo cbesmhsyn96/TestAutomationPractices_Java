4c3b98de7dfc8e3538246015febe398e92eeb8a3
huseyinakcan <system.c.hsyn@gmail.com>
2023-10-20 01:11:11 +0300

    first commit maven_selenium_junit5

---
+++ src/test/java/PracticeTests.java
@@ -0,0 +1,98 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class PracticeTests {
+
+    WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    private static String expectedText = "";
+    @BeforeAll
+    public static void beforeOfAll(){
+        System.out.println("------------------------");
+    }
+    @BeforeEach
+    public void setup(){
+
+        driver.get("https://react-shopping-cart-67954.firebaseapp.com/");
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void productNameCompareWhenAddToCard(){
+        String rootPathsList = "//main/following-sibling::div[@class='sc-uhudcz-0 iZZGui']/div";
+        int random_Index = (int)Math.floor(Math.random() * (16 - 1 + 1) + 1);
+        String randProductRootPath = rootPathsList+"["+random_Index+"]";
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(rootPathsList)));
+        WebElement nameProduct = driver.findElement(By.xpath(rootPathsList+"["+random_Index+"]/div[@alt]/following-sibling::p"));
+        WebElement randAddToCart = driver.findElement(By.xpath(randProductRootPath+"//button"));
+        expectedText = nameProduct.getText();
+        randAddToCart.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='sc-ebmerl-0 foenKb']/div[2]/div/div[2]/div/div[1]/p[1]")));
+        WebElement nameProduct_Cart = driver.findElement(By.xpath("//div[@class='sc-ebmerl-0 foenKb']/div[2]/div/div[2]/div/div[1]/p[1]"));
+        Assertions.assertEquals(expectedText,nameProduct_Cart.getText());
+    }
+    @Test
+    public void compareProductListPriceAndThePriceInTheCart(){
+        String rootPathsList = "//main/following-sibling::div[@class='sc-uhudcz-0 iZZGui']/div";
+        int random_Index = (int)Math.floor(Math.random() * (16 - 1 + 1) + 1);
+        String randProductRootPath = rootPathsList+"["+random_Index+"]";
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(rootPathsList)));
+        WebElement priceProduct = driver.findElement(By.xpath(rootPathsList+"["+random_Index+"]//p[contains(@class,'sc-124al1g-6')]"));
+        expectedText = priceProduct.getText();
+        WebElement randAddToCart = driver.findElement(By.xpath(randProductRootPath+"//button"));
+        randAddToCart.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='sc-ebmerl-0 foenKb']/div[2]/div/div[2]/div/div[2]/p[1]")));
+        WebElement priceProduct_Cart = driver.findElement(By.xpath("//div[@class='sc-ebmerl-0 foenKb']/div[2]/div/div[2]/div/div[2]/p[1]"));
+        String actualTextPrice = priceProduct_Cart.getText();
+        actualTextPrice = actualTextPrice.replace(" ","");
+        Assertions.assertEquals(expectedText,actualTextPrice);
+    }
+    @Test
+    public void clickPlusButtonInTheCart() throws InterruptedException {
+        //Hatalı. Carttaki fiyatların karşılaştırılması lazım.
+        String rootPathsList = "//main/following-sibling::div[@class='sc-uhudcz-0 iZZGui']/div";
+        int random_Index = (int)Math.floor(Math.random() * (16 - 1 + 1) + 1);
+        int randomClickCount = (int)Math.floor(Math.random() * (5 - 1 + 1) + 1);
+        float currentPrice = 0;
+        int currentClickCount = 0;
+        String priceProductValue = "";
+        float priceProductFloat = 0;
+        String randProductRootPath = rootPathsList+"["+random_Index+"]";
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(rootPathsList)));
+        WebElement randAddToCart = driver.findElement(By.xpath(randProductRootPath+"//button"));
+        WebElement priceProduct = driver.findElement(By.xpath(rootPathsList+"["+random_Index+"]//p[contains(@class,'sc-124al1g-6')]"));
+        priceProductValue = priceProduct.getText();
+        priceProductValue = priceProductValue.replace("$","");
+        priceProductFloat = Float.parseFloat(priceProductValue);
+        randAddToCart.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='sc-ebmerl-0 foenKb']/div[2]/div/div[2]/div/div[2]/p[1]")));
+        WebElement priceProduct_Cart = driver.findElement(By.xpath("//div[@class='sc-ebmerl-0 foenKb']/div[2]/div/div[2]/div/div[2]/p[1]"));
+        String currentTextPrice = priceProduct_Cart.getText();
+        currentTextPrice = currentTextPrice.replace("$","");
+        currentPrice = Float.parseFloat(currentTextPrice);
+        WebElement plusButton = driver.findElement(By.xpath("//div[@class='sc-ebmerl-0 foenKb']/div[2]/div/div[2]/div/div[2]/div/button[2]"));
+        System.out.println("+ butonuna "+randomClickCount +" kere tıklanacak.");
+        while(currentClickCount < randomClickCount){
+            plusButton.click();
+            currentClickCount++;
+            System.out.println("+ butonuna "+currentClickCount+" kere tıklandı.");
+            System.out.println("currentPrice*currentClickCount => "+currentPrice*currentClickCount);
+            Assertions.assertEquals(String.valueOf(currentPrice*currentClickCount),String.valueOf(priceProductFloat*currentClickCount));
+            Thread.sleep(5000);
+        }
+    }
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Test sonlandı.");
+        driver.quit();
+    }
+    @AfterAll
+    public static void afterOfAll(){
+        System.out.println("------------------------");
+    }
+}
